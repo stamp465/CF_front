626d06301e36ec2a26af45aca7e1b95c26e4998f
Witnapat <lovelove6985@gmail.com>
2022-11-27 21:03:29 +0700

    edit some bug

--- components/now.js
+++ components/now.js
@@ -1,8 +1,43 @@
 import React from "react";
 import { useEffect } from "react";
-import { bgClass, borderH, borderS, btn } from "../styles/color"
 
 function Now({ unit, addTeam }) {
+
+  const bgClass = {
+    green: `border-2 border-green-900 bg-green-500
+          rounded-tr-xl rounded-bl-xl rounded-tl-3xl rounded-br-3xl
+          overflow-hidden w-[80px] h-[80px] `,
+    blue: `border-2 border-blue-900 bg-blue-500
+          rounded-tr-xl rounded-bl-xl rounded-tl-3xl rounded-br-3xl
+          overflow-hidden w-[80px] h-[80px] `,
+    red: `border-2 border-red-900 bg-red-500
+          rounded-tr-xl rounded-bl-xl rounded-tl-3xl rounded-br-3xl
+          overflow-hidden w-[80px] h-[80px] `,
+    yellow: `border-2 border-yellow-900 bg-yellow-500
+          rounded-tr-xl rounded-bl-xl rounded-tl-3xl rounded-br-3xl
+          overflow-hidden w-[80px] h-[80px] `
+  }
+
+  const borderH = {
+    yellow: "font-bold text-base border-2 border-yellow-600 inline-block px-1 py-0.5 rounded-md",
+    green: "font-bold text-base border-2 border-green-600 inline-block px-1 py-0.5 rounded-md",
+    blue: "font-bold text-base border-2 border-blue-600 inline-block px-1 py-0.5 rounded-md",
+    red: "font-bold text-base border-2 border-red-600 inline-block px-1 py-0.5 rounded-md"
+  }
+
+  const borderS = {
+    yellow: "my-2 text-sm border-2 border-yellow-400 px-1 py-0.5 rounded-md",
+    green: "my-2 text-sm border-2 border-green-400 px-1 py-0.5 rounded-md",
+    blue: "my-2 text-sm border-2 border-blue-400 px-1 py-0.5 rounded-md",
+    red: "my-2 text-sm border-2 border-red-400 px-1 py-0.5 rounded-md"
+  }
+
+  const btn = {
+    blue: "btn btn-outline btn-info border-4 ",
+    green: "btn btn-outline btn-success border-4 ",
+    yellow: "btn btn-outline btn-warning border-4 ",
+    red: "btn btn-outline btn-error border-4 "
+  }
 
   useEffect(() => {
     console.log("unit change")
--- pages/test.js
+++ pages/test.js
@@ -1,28 +1,37 @@
-function test({ tmps }) {
-  console.log(tmps.urlPicture.length)
+// function test({ tmps }) {
+//   console.log(tmps.urlPicture.length)
+//   return (
+//     <div>
+//       <img src={'data:image/png;base64,' + tmps.urlPicture.slice(2, tmps.urlPicture.length - 1)} />
+//       <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4
+//         //8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
+//     </div>
+//   );
+// }
+
+// async function getTexttmp() {
+//   const res = await fetch(process.env.NEXT_API_URL + "/texttmps");
+//   // console.log(res)
+//   return res.json();
+// }
+
+// export async function getStaticProps(ctx) {
+//   const tmps = await getTexttmp()
+
+//   return {
+//     props: {
+//       tmps: tmps.results[0]
+//     }
+//   }
+// }
+
+// export default test;
+function test() {
   return (
     <div>
-      <img src={'data:image/png;base64,' + tmps.urlPicture.slice(2, tmps.urlPicture.length - 1)} />
-      <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4
-        //8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" alt="Red dot" />
+      Enter
     </div>
   );
-}
-
-async function getTexttmp() {
-  const res = await fetch(process.env.NEXT_API_URL + "/texttmps");
-  // console.log(res)
-  return res.json();
-}
-
-export async function getStaticProps(ctx) {
-  const tmps = await getTexttmp()
-
-  return {
-    props: {
-      tmps: tmps.results[0]
-    }
-  }
 }
 
 export default test;
